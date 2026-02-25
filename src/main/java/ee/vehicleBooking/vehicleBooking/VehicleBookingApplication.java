833a55fcd66a5a93bdbc4bd6d40b7a80491cc180
LaXHeXLuX <laas.lumberg@gmail.com>
2026-02-25 17:46:06 +0200

    remove unneeded variables and imports

--- src/main/java/ee/vehicleBooking/vehicleBooking/VehicleBookingApplication.java
+++ src/main/java/ee/vehicleBooking/vehicleBooking/VehicleBookingApplication.java
@@ -1,17 +1,11 @@
 package ee.vehicleBooking.vehicleBooking;
 
 import ee.vehicleBooking.vehicleBooking.model.*;
-import ee.vehicleBooking.vehicleBooking.repository.CarRepository;
-import ee.vehicleBooking.vehicleBooking.repository.ClientRepository;
 import ee.vehicleBooking.vehicleBooking.repository.UnitRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class VehicleBookingApplication {
@@ -21,7 +15,7 @@ public class VehicleBookingApplication {
 	}
 
     @Bean
-    public CommandLineRunner seedData(CarRepository carRepository, UnitRepository unitRepository, ClientRepository clientRepository) {
+    public CommandLineRunner seedData(UnitRepository unitRepository) {
         return _ -> {
             System.out.println("Up and running! Current unit count: " + unitRepository.count());
         };
