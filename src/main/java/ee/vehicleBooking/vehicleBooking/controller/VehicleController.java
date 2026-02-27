f998bdf4c1ffd494f5823ffae7c7d44640b906c0
LaXHeXLuX <laas.lumberg@gmail.com>
2026-02-27 07:48:12 +0200

    api path vehicle -> vehicles

--- src/main/java/ee/vehicleBooking/vehicleBooking/controller/VehicleController.java
+++ src/main/java/ee/vehicleBooking/vehicleBooking/controller/VehicleController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/vehicle")
+@RequestMapping("/api/vehicles")
 public class VehicleController {
     private final VehicleService vehicleService;
 
