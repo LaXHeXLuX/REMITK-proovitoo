52cd1a1686ccd07b4f5690e31f6152673d3012a2
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-01 11:51:01 +0200

    remove client

--- src/main/java/ee/vehicleBooking/vehicleBooking/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.vehicleBooking.vehicleBooking.repository;
-
-import ee.vehicleBooking.vehicleBooking.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
