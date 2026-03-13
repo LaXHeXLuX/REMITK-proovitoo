4355272ef2d0c8aea397bd28fe8b5654e0e7b80e
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-14 01:37:57 +0200

    add misc spec and css files

---
+++ frontend/src/app/pipes/vehicle-description-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { VehicleDescriptionPipe } from './vehicle-description-pipe';
+
+describe('VehicleDescriptionPipe', () => {
+	it('create an instance', () => {
+		const pipe = new VehicleDescriptionPipe();
+		expect(pipe).toBeTruthy();
+	});
+});
